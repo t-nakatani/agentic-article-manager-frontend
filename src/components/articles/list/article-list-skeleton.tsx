b537c4c55cd25fb0300c4e400d6187cb6a6f03c9
t-nakatani <tomoya.12@icloud.com>
2025-03-16 12:23:32 +0900

    feat: skeltonを修正

--- src/components/articles/list/article-list-skeleton.tsx
+++ src/components/articles/list/article-list-skeleton.tsx
@@ -5,7 +5,10 @@ export function ArticleListSkeleton() {
   return (
     <div className="space-y-4">
       {[...Array(4)].map((_, i) => (
-        <Card key={i} className="bg-white dark:bg-theme-900">
+        <Card 
+          key={i} 
+          className="bg-white dark:bg-theme-900 rounded-xl overflow-hidden relative before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-1 before:bg-theme-200/50 dark:before:bg-theme-700/30 border-0 shadow-none"
+        >
           <CardContent className="p-2.5">
             <div className="space-y-0.5">
               <div className="flex items-start justify-between gap-2">
