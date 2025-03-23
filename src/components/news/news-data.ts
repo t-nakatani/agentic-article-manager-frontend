29af2d5c28bf0e3cee5c57ae8c68dc134d9f405d
t-nakatani <tomoya.12@icloud.com>
2025-03-24 01:37:12 +0900

    newsを更新

--- src/components/news/news-data.ts
+++ src/components/news/news-data.ts
@@ -2,6 +2,17 @@ import type { NewsItem } from "@/types/news"
 
 export const newsItems: NewsItem[] = [
   {
+    id: "production-release",
+    title: "一般公開しました",
+    date: "2024-03-24",
+    tags: ["new", "beta"],
+    descriptions: [
+      "アプリケーションを一般公開しました",
+      "ユーザ登録後、拡張機能またはiOSショートカットから記事を保存できます",
+      "カテゴリを作成することで、記事を自動分類できます"
+    ]
+  },
+  {
     id: "ios-shortcut&extension-update",
     title: "iOSショートカットの公開&拡張機能Update",
     date: "2024-03-08",
@@ -11,10 +22,10 @@ export const newsItems: NewsItem[] = [
       "拡張機能にアイコンが追加されました",
       "右クリックから拡張機能で記事を保存できるようになりました"
     ],
-    link: {
-      text: "ショートカットをダウンロード",
-      url: "/extension#ios-shortcut",
-    },
+    // link: {
+    //   text: "ショートカットをダウンロード",
+    //   url: "/extension#ios-shortcut",
+    // },
   },
   {
     id: "alpha-release",
@@ -25,10 +36,10 @@ export const newsItems: NewsItem[] = [
       "chrome拡張機能から記事を保存できるようになりました",
       "保存した記事がユーザ定義のテーマに自動分類されるようになりました"
     ],
-    link: {
-      text: "アルファ版を試す",
-      url: "/extension",
-    },
+    // link: {
+    //   text: "アルファ版を試す",
+    //   url: "/extension",
+    // },
   },
 ]
 
